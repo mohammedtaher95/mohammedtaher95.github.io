ad135cb56714c8f481c8129c4f5fd4bcf267c76f
mohammedtaher95 <mohammedtaher10295@gmail.com>
2024-10-21 18:06:09 +0300

    Handle responsiveness for Header

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const headerItems = document.querySelectorAll('.menu-item');
+const icons = document.querySelectorAll('.svg');
 
 headerItems.forEach(item => {
+    // Mouseover event to change color when hovering
+    try {
+        item.addEventListener('mouseover', function() {
+          this.classList.add('text-wrapper-2');
+        });
+  
+        item.addEventListener('mouseout', function() {
+          this.classList.remove('text-wrapper-2');
+        });
+      } catch (error) {
+        console.error('Error adding event listener for:', item, error);
+      }
+  });
+
+  icons.forEach(item => {
     // Mouseover event to change color when hovering
     try {
         item.addEventListener('mouseover', function() {
